5ac47f93b05c17ab0c28f5942a18015fc45e33dd
hvand9 <54908812+hvand9@users.noreply.github.com>
2022-01-05 02:32:58 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -12,23 +12,42 @@ export default {
   <Sidebar />
   <div :style="{ 'margin-left': sidebarWidth }">
     <router-view />
+
+  <footer>
+    <div class="footer1">
+<a href="https://61d482940327745607ae8534--gamemeet.netlify.app/?fbclid=IwAR2mIY_Y3C7FReT3TvyizcacckQf6eQDL-kdOJsju7UKRNWQE9ievaUNgg8">Forside</a>
+<a href="https://nifty-montalcini-3382e1.netlify.app/">Log ind</a>
+<a href="https://nifty-montalcini-3382e1.netlify.app/">Kontakt</a>
+ <img class="logos" src="./assets/yty.png" width="40px" height="40px">
+ <img class="logos" src="./assets/skzjx.png" width="40px" height="40px">
+ <img class="logos" src="./assets/sjzk.png" width="50px" height="45px">
+<a href="https://nifty-montalcini-3382e1.netlify.app/">Regler</a>
+<a href="https://nifty-montalcini-3382e1.netlify.app/">Om os</a>
+     </div>
+    </footer>
   </div>
   <div id="app">
-        <img src="./assets/Gamemeet.png">
+        <img class="logo" src="./assets/Gamemeet.png">
     </div>
+
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: 'Nunito', sans-serif;
   text-align: center;
   color: #ffffff;
 }
 
 
-img {
+
+
+
+body {background-color: black} 
+
+.logo {
 position: absolute;
 width: 180px;
 height: 90px;
@@ -36,6 +55,23 @@ left: -2px;
 top: 10px;
 
 }
+
+.footer1 {
+  flex-direction: row;
+  display: flex;
+  white-space: initial;
+  justify-content: space-between;
+  padding-bottom: 10px;
+}
+
+a { text-decoration: none; color:aliceblue}
+
+
+
+.footer3 {
+  color: white;
+}
+
 
 #nav {
   padding: 30px;
@@ -50,3 +86,4 @@ top: 10px;
   color: #42b983;
 }
 </style>
+
